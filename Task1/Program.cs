c3cf0c031abf4d0e7586699a42535975b996870b
Evgeniia Briukhova <eve.briukhova897@gmail.com>
2024-05-19 11:53:55 +0600

    changes01 in Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,46 +3,43 @@
 при вводе символа ‘q’ или при вводе числа, сумма цифр 
 которого чётная. */
 
-
-
-    
-        while (true)
+while (true)
         {
             Console.Write("Input number (or 'q' for exit): ");
             string input = Console.ReadLine();
 
             if (input == "q")
             {
-                Console.WriteLine("End of the programm.");
+                Console.WriteLine("End of the program.");
                 break;
             }
         
-      if (int.TryParse(input, out int number))
-      {
-            if (IsSumOfDigitsEven(number))
+            if (int.TryParse(input, out int number))
             {
-                Console.WriteLine("Sum of numbers are even. End of the program");
-                break;
+                if (IsSumOfDigitsEven(number))
+                {
+                    Console.WriteLine("Sum of digits is even. End of the program.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Sum of digits is odd. Continue to input numbers.");
+                }
             }
             else
             {
-                Console.WriteLine("Sum of numbers are odd. Continue to input numbers.");
+                Console.WriteLine("Input error. Please, input a number or 'q' for exit.");
             }
         }
-            else
-        {
-            Console.WriteLine("Input error. Please, please input number or 'q' for exit.");
-        }
-    }
     
-
-static bool IsSumOfDigitsEven(int number)
-{
-    int sum = 0;
-    while (number > 0)
+    
+    static bool IsSumOfDigitsEven(int number)
     {
-        sum += number % 10;
-        number /= 10;
+        int sum = 0;
+        while (number != 0)
+        {
+            sum += Math.Abs(number % 10); // Используем Math.Abs для обработки отрицательных чисел
+            number /= 10;
+        }
+        return sum % 2 == 0;
     }
-    return sum % 2 == 0;
-}
