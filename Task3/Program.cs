4ec1025f422dc47718a5f91fd83597f4bf51ac48
Evgeniia Briukhova <eve.briukhova897@gmail.com>
2024-05-19 12:12:14 +0600

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,54 @@
+﻿/* Задача 3: Напишите программу, которая перевернёт 
+одномерный массив (первый элемент станет последним, 
+второй – предпоследним и т.д.) */
+
+int[] CreateArrayRndInt(int size, int min, int max) 
+{
+    int[] array = new int[size];  
+    Random rnd = new Random(); 
+
+    for (int i = 0; i < size; i++) 
+    {
+        array[i] = rnd.Next(min, max); 
+    }
+    return array; 
+}
+
+void PrintArray(int[] array) 
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i < array.Length - 1) 
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+        Console.Write($"{array[i]}");
+        }
+    }
+    Console.Write("]");
+}
+
+
+void ReverseArray(int[] array)
+{
+    int left = 0;
+    int right = array.Length -1;
+    while (left < right)
+        {
+            int temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+            left++;
+            right--;
+        }
+    }
+
+
+int[]arr = CreateArrayRndInt(10, 1, 100); 
+PrintArray(arr);
+Console.WriteLine();
+ReverseArray(arr);
+PrintArray(arr);
